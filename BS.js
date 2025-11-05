0ec27f4d947ea4e56ce7d746ca90f21bef7feddf
Garv Goel <goelgarv99@gmail.com>
2025-11-05 16:13:41 +0530

    parsing of csv

--- BS.js
+++ BS.js
@@ -1,2 +1,14 @@
-const fs = require("fs");
-let data = fs.readFile("./fe02_bank.csv", 'utf-8');
+const fs = require("fs").promises;
+async function analyzeCSV() {
+    const data = await fs.readFile("./fe02_bank.csv", "utf-8");
+    const lines = data.trim().split("\n");
+    const header = lines[0].split(",").map(head => head.trim());
+    const transactions = lines.slice(1).map(line => {
+      const values = line.split(",").map(value => value.trim());
+      const obj = {};
+      header.forEach((head, i) => {
+        obj[head] = values[i];
+      });
+      return obj;
+    });
+}
