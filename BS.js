eac5d4043fdc3e9e3920eb74d1c7a6b7dc1e9b2b
Garv Goel <goelgarv99@gmail.com>
2025-11-05 16:34:15 +0530

    Added the summary

--- BS.js
+++ BS.js
@@ -1,8 +1,11 @@
 const fs = require("fs").promises;
+
 async function analyzeCSV() {
+  try {
     const data = await fs.readFile("./fe02_bank.csv", "utf-8");
     const lines = data.trim().split("\n");
     const header = lines[0].split(",").map(head => head.trim());
+
     const transactions = lines.slice(1).map(line => {
       const values = line.split(",").map(value => value.trim());
       const obj = {};
@@ -11,6 +14,50 @@ async function analyzeCSV() {
       });
       return obj;
     });
+
+    const sorted_data = transactions.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+    const summ_arr = {};
+
+    sorted_data.forEach(element => {
+      const name = element.AccountHolder;
+      const amount = Number(element.Amount);
+      const type = element.Type.toLowerCase();
+      const remark = element.Remarks.toLowerCase();
+
+      if (!summ_arr[name]) {
+        summ_arr[name] = {
+          AccountHolder: name,
+          TotalCredit: 0,
+          TotalDebit: 0,
+          LargestTransaction: 0,
+          SalaryTransactions: []
+        };
+      }
+
+      if (type === "credit") summ_arr[name].TotalCredit += amount;
+      else if (type === "debit") summ_arr[name].TotalDebit += amount;
+
+      if (amount > summ_arr[name].LargestTransaction) {
+        summ_arr[name].LargestTransaction = amount;
+      }
+
+      if (remark.includes("salary")) {
+        summ_arr[name].SalaryTransactions.push(element.TransactionID);
+      }
+    });
+
+    const summary_array = Object.values(summ_arr);
+    const headerRow = "AccountHolder,TotalCredit,TotalDebit,LargestTransaction,SalaryTransactions";
+    let csv = headerRow + "\n";
+
+    for (let i = 0; i < summary_array.length; i++) {
+      const user = summary_array[i];
+      csv += `${user.AccountHolder},${user.TotalCredit},${user.TotalDebit},${user.LargestTransaction},"${user.SalaryTransactions.join(";")}"\n`;
+    }
+
+    await fs.writeFile("./bank_summary.csv", csv, "utf-8");
+  } catch (err) {
+    console.error("Error:", err);
+  }
 }
-  const sorted_data = transactions.sort((a, b) => new Date(a.Date) - new Date(b.Date));
-  
+analyzeCSV();
